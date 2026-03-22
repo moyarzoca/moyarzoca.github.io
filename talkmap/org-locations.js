4c2ea4a1557ce3d44dfab93a00852ec0c667348e
Marcelo Oyarzo <moyarzoca1@gmail.com>
2026-03-22 16:49:25 -0300

    Update talkmap locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,13 +1,18 @@
 var addressPoints = [
   [
+    "\u00bfQu\u00e9 hay en el interior de los agujeros negros?<br />Liceo Rodulfo Amando Philippi de Paillaco; Paillaco, Chile",
+    -40.0706633,
+    -72.8726695
+  ],
+  [
     "Perturbative higher curvature corrections of black holes in string-inspired scenarios<br />Talk at Cosmology and Particles 2022 UBB, Chillan; Chillan, Chile",
     -36.6066616,
     -72.1033194
   ],
   [
     "Perturbative higher curvature corrections of black holes and black strings<br />Seminar at Centre de Physique Th\u00e9orique; Paris, France",
-    48.8534951,
-    2.3483915
+    48.8588897,
+    2.320041
   ],
   [
     "Supersymmetric AdS solitons and phase transitions in N = 4 SYM<br />Seminar at Swansea University, Swansea; Swansea, Wales",
@@ -50,6 +55,11 @@ var addressPoints = [
     -70.6510671
   ],
   [
+    "Neural Networks and Rotating Black Holes in Higher-Curvature Gravities<br />Universidad de Concepci\u00f3n; Santiago de Compostela, Spain",
+    42.8804219,
+    -8.5458608
+  ],
+  [
     "Gravitational instantons with conformally coupled scalar fields<br />Seminar at theory group of Universidad de Buenos Aires; Buenos Aires, Argentina",
     -34.6095579,
     -58.3887904
@@ -75,11 +85,6 @@ var addressPoints = [
     -46.6333824
   ],
   [
-    "Perturbative higher curvature corrections of black holes and black strings<br />Centra, Universidad de Lisbon; Lisbon, Portugal",
-    38.7077507,
-    -9.1365919
-  ],
-  [
     "An holographic perspective of a confining field theory in type IIB<br />Seminar at Politecnico di Torino; Torino, Italy",
     45.0677551,
     7.6824892
@@ -88,5 +93,70 @@ var addressPoints = [
     "Quadratic gravity and conformally coupled scalar fields<br />Escuela de gravitaci\u00f3n y Relatividad General; Valdivia, Chile",
     -39.8141262,
     -73.2459859
+  ],
+  [
+    "Confining and deconfining backgrounds in type IIB supergravity<br />NordGrav@ICEN UNAP, Iquique; Iquique, Chile",
+    -20.2140657,
+    -70.1524646
+  ],
+  [
+    "Black Holes in the STU Model of Gauged D=4 N=8 SUGRA<br />ICC Universitat de Barcelona, Spain; Barcelona, Spain",
+    41.3825802,
+    2.177073
+  ],
+  [
+    "Hablemos del tiempo: desde la concepci\u00f3n Newtoniana a la Relatividad General<br />Liceo Rodulfo Amando Philippi de Paillaco; Paillaco, Chile",
+    -40.0706633,
+    -72.8726695
+  ],
+  [
+    "Stability of Black Holes in the STU Model of D=4, N=8Gauged Supergravity<br />IGFAE, Universidade de Santiago de Compostela; Santiago de Compostela, Spain",
+    42.8804219,
+    -8.5458608
+  ],
+  [
+    "An holographic perspective of a (1+1) confining field theory<br />Seminar at Haifa University; Haifa, Israel",
+    32.8191218,
+    34.9983856
+  ],
+  [
+    "Perturbative higher curvature corrections of black holes and black strings<br />Seminar at Swansea University; Swansea, Wales",
+    51.6195955,
+    -3.9459248
+  ],
+  [
+    "An holographic perspective of a (1+1) confining field theory<br />Seminar at Udec Gravity Group; Concepci\u00f3n, Chile",
+    -36.8270698,
+    -73.0502064
+  ],
+  [
+    "Gravitational instantons with conformally coupled scalar fields<br />Seminar at gravity group of Universidad de Concepci\u00f3n; Concepci\u00f3n, Chile",
+    -36.8270698,
+    -73.0502064
+  ],
+  [
+    "Perturbative higher curvature corrections of black holes and black strings<br />Seminar at Universit\u00e9 Libre de Bruxelles; Brussels, Belgium",
+    50.8467372,
+    4.352493
+  ],
+  [
+    "Supersymmetric AdS solitons in type IIB<br />Universita di Torino, Torino; Torino, Italy",
+    45.0677551,
+    7.6824892
+  ],
+  [
+    "Black holes and solitons in supergravity<br />50 year of dimensional regularization; La Plata, Argentina",
+    -34.9206797,
+    -57.9537638
+  ],
+  [
+    "Perturbative higher curvature corrections of black holes and black strings<br />Centra, Universidad de Lisbon; Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Black Holes in the STU Model of Gauged D=4 N=8 SUGRA<br />Universt\u00e9 libre de Bruxelles; Brussels, Belgium",
+    50.8467372,
+    4.352493
   ]
 ];
